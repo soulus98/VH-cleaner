39d6940201791e3f8144da1643f12f9aa3d67a8a
Soul-Home <soulus98@hotmail.com>
2022-06-20 09:03:22 +1000

    test

--- commands/cleanVerified.js
+++ commands/cleanVerified.js
@@ -7,31 +7,36 @@ module.exports = {
 	permissions: "ADMINISTRATOR",
 	type:"Admin",
 	async execute(message) {
-		console.log(ops.VH);
-		const vh = await message.guild.roles.fetch(ops.VH);
-		const rr = await message.guild.roles.fetch(ops.RR);
-		message.reply(`Remote Raids = ${rr.size}
-Verified Host = ${vh.size}
-Approximately ${vh.size - rr.size} members will lose the Verified Host role.
+		message.reply("Loading members");
+		message.guild.members.fetch().then((members) =>{
+			message.reply("Members loaded. Size = ", members.size);
+			const vh = await message.guild.roles.fetch(ops.VH);
+			const rr = await message.guild.roles.fetch(ops.RR);
+			message.reply(`Remote Raids = ${rr.members.size}
+Verified Host = ${vh.members.size}
+Approximately ${vh.members.size - rr.members.size} members will lose the Verified Host role.
 React with ✅ to continue, ❌ to cancel.
 This message will last 60 seconds.`).then((msg) => {
-			msg.react("✅").then(() => msg.react("❌"));
-			const filter = (reaction, usr) => {
-				return ["✅", "❌"].includes(reaction.emoji.name) && usr.id === message.author.id;
-			};
-			msg.awaitReactions({ filter, max: 1, time: 60000, errors: ["time"] }).then((collected) => {
-				console.log("testo 1");
-				if (collected.first().emoji.name === "✅") {
+				msg.react("✅").then(() => msg.react("❌"));
+				const filter = (reaction, usr) => {
+					return ["✅", "❌"].includes(reaction.emoji.name) && usr.id === message.author.id;
+				};
+				msg.awaitReactions({ filter, max: 1, time: 60000, errors: ["time"] }).then((collected) => {
+					console.log("testo 1");
+					if (collected.first().emoji.name === "✅") {
+						msg.delete();
+						message.reply("test success");
+					} else {
+						msg.delete();
+						message.reply("test declined");
+					}
+				}).catch(() => {
 					msg.delete();
-					message.reply("test success");
-				} else {
-					msg.delete();
-					message.reply("test declined");
-				}
-			}).catch(() => {
-				msg.delete();
-				message.reply("test expired");
+					message.reply("test expired");
+				});
 			});
+		}).catch((err) =>{
+			message.reply("Sorry, I didn't load in time... :(\n",err);
 		});
 	},
 };
