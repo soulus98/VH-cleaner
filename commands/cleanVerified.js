138e8b32eb8328d042f265cfe6ad283192e5ada6
Soul <soulus98@hotmail.com>
2022-06-20 17:12:20 +1000

    Working test

--- commands/cleanVerified.js
+++ commands/cleanVerified.js
@@ -1,3 +1,5 @@
+/* eslint-disable max-nested-callbacks*/
+
 module.exports = {
 	name: "cleanup-verified",
 	description: "Deletes VH from anyone who has VH and not RR",
@@ -7,37 +9,55 @@ module.exports = {
 	permissions: "ADMINISTRATOR",
 	type:"Admin",
 	async execute(message) {
-		message.reply("Loading members");
-		message.guild.members.fetch().then(async (members) =>{
-			message.reply("Members loaded. Size = ", members.size);
-			const vh = await message.guild.roles.fetch(ops.VH);
-			const rr = await message.guild.roles.fetch(ops.RR);
-			message.reply(`Remote Raids = ${rr.members.size}
+		message.reply("Loading members").then((loadingMessage) => {
+			message.guild.members.fetch().then(async (members) => {
+				loadingMessage.edit(`Members loaded. Size = ${members.size}`);
+				const vh = await message.guild.roles.fetch(ops.VH);
+				const rr = await message.guild.roles.fetch(ops.RR);
+				message.reply(`Remote Raids = ${rr.members.size}
 Verified Host = ${vh.members.size}
 Approximately ${vh.members.size - rr.members.size} members will lose the Verified Host role.
 React with ✅ to continue, ❌ to cancel.
 This message will last 60 seconds.`).then((msg) => {
-				msg.react("✅").then(() => msg.react("❌"));
-				const filter = (reaction, usr) => {
-					return ["✅", "❌"].includes(reaction.emoji.name) && usr.id === message.author.id;
-				};
-				msg.awaitReactions({ filter, max: 1, time: 60000, errors: ["time"] }).then((collected) => {
-					console.log("testo 1");
-					if (collected.first().emoji.name === "✅") {
+					msg.react("✅").then(() => msg.react("❌"));
+					const filter = (reaction, usr) => {
+						return ["✅", "❌"].includes(reaction.emoji.name) && usr.id === message.author.id;
+					};
+					msg.awaitReactions({ filter, max: 1, time: 60000, errors: ["time"] }).then((collected) => {
+						if (collected.first().emoji.name === "✅") {
+							loadingMessage.delete();
+							msg.delete();
+							message.reply("Processing: 0%").then(processingMessage => {
+								const cheaters = vh.members.filter(memb => !memb.roles.cache.has(ops.RR));
+								const size = cheaters.size;
+								for (let i = 0; i < size; i++) {
+									const member = cheaters.at(i);
+									if (member.roles.cache.has(ops.RR)) console.log(member, "#", member.id, "had both VH and RR");
+									else {
+										setTimeout(() => {
+											if (i % 10 == 0 && i != 0) processingMessage.edit(`Processing: ${((i / (size - 1)) * 100).toFixed(2)}%`);
+											// rem role
+											member.roles.remove(ops.VH);
+											console.log(`Removed VH from ${member.user.username}#${member.id}`);
+											if (i == size - 1) return processingMessage.edit(`Completed.\nChecked ${members.size} members and removed VH ${i} times`);
+										}, 4000 * i);
+									}
+								}
+							});
+						} else {
+							loadingMessage.delete();
+							msg.delete();
+						}
+					}).catch(() => {
+						loadingMessage.delete();
 						msg.delete();
-						message.reply("test success");
-					} else {
-						msg.delete();
-						message.reply("test declined");
-					}
-				}).catch(() => {
-					msg.delete();
-					message.reply("test expired");
+					});
 				});
+			}).catch((err) => {
+				message.reply("Sorry, I didn't load in time... :(");
+				message.reply(err);
+				console.error(err);
 			});
-		}).catch((err) => {
-			message.reply("Sorry, I didn't load in time... :(");
-			console.error(err);
 		});
 	},
 };
